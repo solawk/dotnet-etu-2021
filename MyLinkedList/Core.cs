8a31027c3477c3a59ae414557d95975c598f2c99
Solawk <fulmenius@yandex.ru>
2021-02-11 16:40:00 +0300

    Project created

---
+++ MyLinkedList/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLinkedList
+{
+    class Core
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
