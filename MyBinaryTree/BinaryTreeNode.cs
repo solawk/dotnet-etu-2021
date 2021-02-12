7be686d648b1a108b0475a0f936e6e73901307b8
Solawk <fulmenius@yandex.ru>
2021-02-12 13:16:32 +0300

    MyBinaryTree created
    1202-1316

---
+++ MyBinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBinaryTree
+{
+    class BinaryTreeNode
+    {
+        private int _value;
+        private int _key;
+
+        private BinaryTreeNode _leftChild;
+        private BinaryTreeNode _rightChild;
+
+        public BinaryTreeNode(int value, int key)
+        {
+            _value = value;
+            _key = key;
+
+            _leftChild = null;
+            _rightChild = null;
+        }
+
+        public void CreateChild(int value, int key, bool isLeft)
+        {
+            if (isLeft)
+            {
+                _leftChild = new BinaryTreeNode(value, key);
+            }
+            else
+            {
+                _rightChild = new BinaryTreeNode(value, key);
+            }
+        }
+
+        public int GetValue()
+        {
+            return _value;
+        }
+
+        public void SetValue(int newValue)
+        {
+            _value = newValue;
+        }
+    }
+}
---
+++ MyBinaryTree/Core.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBinaryTree
+{
+    class Core
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
