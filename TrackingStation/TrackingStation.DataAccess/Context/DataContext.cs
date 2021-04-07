866c8ad591d295118f9603764afec8c18530cdff
Alexander <37877585+solawk@users.noreply.github.com>
2021-04-07 16:36:36 +0300

    Update DataContext.cs

--- TrackingStation/TrackingStation.DataAccess/Context/DataContext.cs
+++ TrackingStation/TrackingStation.DataAccess/Context/DataContext.cs
@@ -23,31 +23,5 @@ namespace TrackingStation.DataAccess.Context
 
         public virtual DbSet<BodyEntity> Body { get; set; }
         public virtual DbSet<VesselEntity> Vessel { get; set; }
-
-        /*
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Body>(entity =>
-            {
-                entity.Property(e => e.Name).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
-
-                //entity.HasConstraintName("FK_Body_Body");
-            });
-
-            modelBuilder.Entity<Vessel>(entity =>
-            {
-                entity.Property(v => v.Name).IsRequired();
-
-                entity.HasOne(b => b.RefBody)
-                .WithMany(v => v.Vessel);
-
-                //entity.HasConstraintName("FK_Vessel_Body");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
-        }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-        */
     }
 }
